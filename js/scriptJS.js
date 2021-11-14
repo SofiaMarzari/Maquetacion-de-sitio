21554430a6045b7f4a6862e6c36b5867070f3827
Sofia Marzari <sofiamarzari3544@gmail.com>
2021-11-13 23:48:44 -0300

    Trabajando en responsive - Mobile

---
+++ js/scriptJS.js
@@ -0,0 +1,5 @@
+"Use strict";
+document.addEventListener('DOMContentLoaded', function() {
+    let sectionMenuMobile = document.getElementById('section-menu-desplegable');
+    sectionMenuMobile.classList.add("is-hidden");
+});
