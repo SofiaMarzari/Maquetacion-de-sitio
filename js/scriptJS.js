c8e3adc690eca4ca9840874d1a5f1bbe97b5a96d
Sofia Marzari <sofiamarzari3544@gmail.com>
2021-11-14 20:33:15 -0300

    Finalizado el Mobile

--- js/scriptJS.js
+++ js/scriptJS.js
@@ -1,5 +1,31 @@
 "Use strict";
 document.addEventListener('DOMContentLoaded', function() {
-    let sectionMenuMobile = document.getElementById('section-menu-desplegable');
-    sectionMenuMobile.classList.add("is-hidden");
+    $('#section-menu-desplegable').addClass("ocultar");
+    $('#section-btn-abrirmenu').addClass("ocultar");
 });
+
+
+/*          NAVEGACION MOBILE        */
+
+/*DESPLEGAR MENU*/
+$('#img-btn-abrirmenu').on('click', function() {
+    $(this).addClass("ocultar");
+    $('#section-menu-desplegable').removeClass("ocultar");
+    $('#img-btn-cerrarmenu').removeClass("ocultar");
+    $('#section-menu-desplegable').children('ul').removeClass("ocultar");
+
+    $('#section-menu-desplegable').addClass("menu-desplegable");
+});
+$('#section-menu-desplegable').on('click', cerrarmenu);
+/*CERRAR MENU*/
+$('#img-btn-cerrarmenu').on('click', cerrarmenu);
+
+function cerrarmenu() {
+    $('#section-menu-desplegable').removeClass("menu-desplegable");
+    $('#img-btn-cerrarmenu').removeClass("btn-cerrarmenu");
+
+    $('#img-btn-abrirmenu').removeClass("ocultar");
+    $('#section-menu-desplegable').addClass("ocultar");
+    $(this).addClass("ocultar");
+    $('#section-menu-desplegable').children('ul').addClass("ocultar");
+}
