c691680e22a82f29ebc2f6969f92460d57797239
Sofia Marzari <sofiamarzari3544@gmail.com>
2021-11-15 15:08:12 -0300

    Realizada la navegacion con JavaScript- Desktop y Mobile

--- js/scriptJS.js
+++ js/scriptJS.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     $('#section-menu-desplegable').addClass("ocultar");
     $('#section-btn-abrirmenu').addClass("ocultar");
 });
-
+let contenedor = document.getElementById('contenedor-ajax');
 
 /*          NAVEGACION MOBILE        */
 
@@ -27,4 +27,64 @@ function cerrarmenu() {
     $('#section-menu-desplegable').addClass("ocultar");
     $(this).addClass("ocultar");
     $('#section-menu-desplegable').children('ul').addClass("ocultar");
+}
+/*          NAVEGACION DESKTOP Y MOBILE      */
+
+/*      AJAX - PARTIAL RENDER */
+$('.btn-nav-home').on('click', loadHome);
+$('.btn-nav-tendencia').on('click', loadTendencias);
+$('.btn-nav-blog').on('click', loadBlog);
+
+function loadHome(event) {
+    event.preventDefault();
+    fetch('../Aenima_Prueba_Maquetador/home.html').then(
+        function(response) {
+            if (response.ok) {
+                response.text().then(
+                    function(text) {
+                        contenedor.innerHTML = text;
+                        cerrarmenu();
+                    }
+                )
+            }
+        }
+    ).catch(function(response) {
+        contenedor.innerHTML = '<h1>Error- Fallo conexión</h1>';
+    })
+}
+
+function loadTendencias(event) {
+    event.preventDefault();
+    fetch('../Aenima_Prueba_Maquetador/tendencias.html').then(
+        function(response) {
+            if (response.ok) {
+                response.text().then(
+                    function(text) {
+                        contenedor.innerHTML = text;
+                        cerrarmenu();
+                    }
+                )
+            }
+        }
+    ).catch(function(response) {
+        contenedor.innerHTML = '<h1>Error- Fallo conexión</h1>';
+    })
+}
+
+function loadBlog(event) {
+    event.preventDefault();
+    fetch('../Aenima_Prueba_Maquetador/blog.html').then(
+        function(response) {
+            if (response.ok) {
+                response.text().then(
+                    function(text) {
+                        contenedor.innerHTML = text;
+                        cerrarmenu();
+                    }
+                )
+            }
+        }
+    ).catch(function(response) {
+        contenedor.innerHTML = '<h1>Error- Fallo conexión</h1>';
+    })
 }
